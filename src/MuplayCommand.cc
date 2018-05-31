1d87ac29569a90eeea22277deb10392da064b567
Ant <ant@cs.columbia.edu>
2018-05-31 17:48:26 -0400

    small modification just to make sure rr recognizes my changes

---
+++ src/MuplayCommand.cc
@@ -0,0 +1,73 @@
+/**
+* Ant edit lets see if you can even get this code to execute
+*/
+
+/* IDK what imports are actually needed */
+#include <inttypes.h>
+
+#include <limits>
+#include <unordered_map>
+
+#include "preload/preload_interface.h"
+
+#include "AddressSpace.h"
+#include "Command.h"
+#include "TraceStream.h"
+#include "core.h"
+#include "kernel_metadata.h"
+#include "log.h"
+#include "main.h"
+#include "util.h"
+/* ------------------------ */
+
+using namespace std;
+
+namespace rr {
+
+  class MuplayCommand : public Command {
+  public:
+    virtual int run(vector<string>& args) override;
+
+  protected:
+    MuplayCommand(const char* name, const char* help) : Command(name, help) {}
+
+    static MuplayCommand singleton;
+  };
+
+  MuplayCommand MuplayCommand::singleton(
+    "muplay",
+    " rr muplay [trace_dir] [run_cmd] [old_code] [new_code]"
+  );
+
+  struct MuplayFlags {
+    bool some_flag;
+
+    MuplayFlags()
+    : some_flag(false) {}
+  };
+
+  static void muplay(const string& trace_dir, const MuplayFlags& flags,
+                     const vector<string>& args, FILE* out) {
+      if (flags.some_flag)
+        printf("some_flag is true");
+      while (!args.empty())
+        printf("Args are not empty");
+      TraceReader trace(trace_dir);
+      fprintf(out,"You made it to the muplay command!");
+  }
+
+  int MuplayCommand::run(vector<string>& args) {
+    MuplayFlags flags;
+    // TODO CODE TO PARSE OPTIONS
+
+    string trace_dir;
+    if (!parse_optional_trace_dir(args, &trace_dir))
+    {
+      printf("Problem parsing the optional trace_dir");
+      return 1;
+    }
+
+    muplay(trace_dir, flags, args, stdout);
+    return 0;
+  }
+} // namespace rr
